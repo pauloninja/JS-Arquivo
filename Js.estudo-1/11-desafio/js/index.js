26a9c3ce487193c8553658c2ed76bc37ce33570f
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2022-12-16 15:28:28 -0300

    JS

--- Js.estudo-1/11-desafio/js/index.js
+++ Js.estudo-1/11-desafio/js/index.js
@@ -1,15 +1,21 @@
 var idade = prompt('Digite um numero')
+var idade = parseFloat(idade)
 
-if(idade <=15){
-    document.write('Crianca');
-}else if(idade >= 15){
+if(idade <= 0 || idade <= 15){
+   document.write('Crianca');
+}else if(idade == 16 || idade <= 30){
     document.write('Jovem');
-}else(idade >= 30 || idade <= 60);{
-    document.write('Adulto');
-}if(idade >= 60){
+}else(idade >= 31 || idade == 60 );{
+    document.write('Adulto'); 
+}if(idade > 60 || idade == 61){
     document.write('Idoso');
-}
+} 
 
+
+
+
+
+  
 
 
 
---
+++ Js.estudo-1/13-operadores-Ari/js/index.js
@@ -0,0 +1,39 @@
+//var num1 = 9;
+//var num2 = 2;
+
+/*var num1 = prompt('Digite um valor');
+var num2 = prompt('Digite outro valor');
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+document.write('A soma entre ' + num1 + ' e ' +  num2  + '  é: '  + (num1 + num2) + '<br/>');
+document.write('A subtração entre ' + num1 + ' e ' +  num2  + '  é: '  + (num1 - num2) + '<br/>');
+document.write('A multiplicação entre ' + num1 + ' e ' +  num2  + '  é: '  + (num1 * num2) + '<br/>');
+document.write('A divisão entre ' + num1 + ' e ' +  num2  + '  é: '  + (num1 / num2) + '<br/>');
+document.write('A resto da divisão entre ' + num1 + ' e ' +  num2  + '  é: '  + (num1 % num2) + '<br/>');
+document.write('A potencia entre ' + num1 + ' e ' +  num2  + '  é: '  + (num1 ** num2) + '<br/>');
+document.write('O incremento de ' + num1 + ' e: ' + (num1++) + '<br/>') */;//pos
+//document.write(num1)//somente a pos a instrução ser executada
+//document.write('O incremento de ' + num1 + ' e: ' + (++num1) + '<br/>');//pre
+//document.write('O incremento de ' + num1 + ' e: ' + (num1--) + '<br/>');//pos
+//document.write(num1)//pos
+//document.write(--num1)
+//document.write(num1)
+
+//Operadores na pratica
+
+var teste = 10;
+teste /= 5 ;
+document.write(teste) + '<br/>';// += -+ *= /= %= **=
+
+var test = ' olá ';
+test += ' Jorge ';
+document.write(test);
+
+//Precedencia de Execução
+document.write(11 + 5 - (5 * 2) / 2);//5 * 2 / 2 11 + 5 -5 = 11
+//()- aplica precedencia
+//1 lugar multiplicação e subtração
+//2 lugar soma e subtração
+
